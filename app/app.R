53ef59dab4bfc76b68342434b6ac0e5fc83a25a3
Sambou Diao <sam.diao90@gmail.com>
2026-01-15 23:00:49 +0100

    deploy

---
+++ app/app.R
@@ -0,0 +1,41 @@
+library(shiny)
+library(plotly)
+
+ui <- fluidPage(
+  titlePanel("Test ShinyLive"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        "n",
+        "Nombre de points",
+        min = 10,
+        max = 200,
+        value = 50
+      )
+    ),
+    
+    mainPanel(
+      plotlyOutput("plot")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  output$plot <- renderPlotly({
+    set.seed(1)
+    
+    x <- rnorm(input$n)
+    y <- cumsum(x)
+    
+    plot_ly(
+      x = seq_along(y),
+      y = y,
+      type = "scatter",
+      mode = "lines"
+    )
+  })
+}
+
+shinyApp(ui, server)
